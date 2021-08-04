27a478e4255342a6289c73c4bb784a5811010182
nickdeans <nickabramowicz@gmail.com>
2021-08-03 17:56:58 -0700

    created props and button

---
+++ task-list/src/components/Header.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title }) => {
+    const onClick = (e) => {
+        console.log('Click')
+    }
+
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Hello' onClick={onClick} />
+            <Button color='red' text='Hello 2' />
+            <Button color='blue' text='Hello 3' />
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+export default Header
